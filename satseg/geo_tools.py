5fbd12a59e3e86c2427f09f908c0eb5332a6b5de
Dilith Jayakody <dilithjay@gmail.com>
2023-04-17 15:56:23 +0530

    Initial Commit

---
+++ satseg/geo_tools.py
@@ -0,0 +1,99 @@
+import rasterio
+import rasterio.mask
+from rasterio.crs import CRS
+from rasterio.features import rasterize
+
+import geopandas as gpd
+from shapely.geometry import Polygon, box
+from shapely.ops import unary_union
+
+import numpy as np
+
+
+# function of generating binary mask
+def generate_mask(raster_path, shape_path):
+    """Function that generates a binary mask from a vector file (shp or geojson)
+    
+    raster_path = path to the .tif
+    shape_path = path to the shapefile or GeoJson
+    """
+    
+    # Load raster    
+    with rasterio.open(raster_path, "r") as src:
+        raster_img = src.read()
+        raster_meta = src.meta
+    
+    # Load shapefile as a GeoDataFrame
+    train_df = gpd.read_file(shape_path)
+    
+    # Verify crs
+    if train_df.crs != src.crs:
+        print(" Raster crs : {}, Vector crs : {}.\n Convert vector and raster to the same CRS.".format(src.crs,train_df.crs))
+        
+        
+    # Function that generates the mask
+    def poly_from_utm(polygon, transform):
+        poly_pts = []
+
+        poly = unary_union(polygon)
+        for i in np.array(poly.exterior.coords):
+
+            poly_pts.append(~transform * tuple(i))
+
+        new_poly = Polygon(poly_pts)
+        return new_poly
+    
+    
+    poly_shp = []
+    im_size = (src.meta['height'], src.meta['width'])
+    for _, row in train_df.iterrows():
+        if row['geometry'].geom_type == 'Polygon':
+            poly = poly_from_utm(row['geometry'], src.meta['transform'])
+            poly_shp.append(poly)
+        else:
+            for p in row['geometry']:
+                poly = poly_from_utm(p, src.meta['transform'])
+                poly_shp.append(poly)
+
+    mask = rasterize(shapes=poly_shp,
+                     out_shape=im_size)
+    
+    mask = mask.astype("uint16")
+    
+    bin_mask_meta = src.meta.copy()
+    bin_mask_meta.update({'count': 1})
+    
+    return mask
+        
+
+def tif2np(tif_path: str) -> np.ndarray:
+    ds = rasterio.open(tif_path)
+    return ds.read()
+
+
+def get_tif_bounds(tif_path: str, target_crs: int = None):
+    ds = rasterio.open(tif_path)
+    bounds = ds.bounds
+    
+    if target_crs:
+        bounds = rasterio.warp.transform_geom(
+            ds.crs,
+            CRS.from_epsg(target_crs),
+            box(*bounds)
+        )
+    
+    return bounds
+
+
+def pixel_to_crs(pixel_coords: np.ndarray, pixel_bounds: tuple, crs_bounds: tuple):
+    """Convert coordinates from pixel domain to CRS domain
+
+    Args:
+        pixel_coords (np.ndarray): A numpy array of shape (n, 2) where n is the number of coordinates
+        pixel_bounds (tuple): A tuple of 2 integers denoting the maximum number of pixels in each dimension (x, y)
+        crs_bounds (tuple): A tuple of 4 floats denoting the bounds of the tif file in CRS coordinates (left, bottom, right, top)
+    """
+    x, y = pixel_coords[:, 0], pixel_coords[:, 1]
+    
+    x = x / pixel_bounds[0] * (crs_bounds[2] - crs_bounds[0]) + crs_bounds[0]
+    y = (1 - y / pixel_bounds[1]) * (crs_bounds[3] - crs_bounds[1]) + crs_bounds[1]
---
+++ satseg/model.py
@@ -0,0 +1,85 @@
+import os
+import numpy as np
+import segmentation_models_pytorch as smp
+import torch
+from tqdm import tqdm
+from torch.utils.data import Dataset
+from torch import nn, optim
+from torchgeo.models import resnet50, ResNet50_Weights
+
+from satseg.dataset import create_dataloader
+from satseg.utils import device, jaccard_index
+
+
+def create_model(arch: str, channels: int) -> nn.Module:
+    if arch == "unet":
+        model = smp.Unet(
+            encoder_name="resnet50",
+            in_channels=channels,
+            classes=1,
+        )
+        model_resnet = resnet50(ResNet50_Weights.SENTINEL2_ALL_MOCO)
+        del model_resnet.fc
+        del model_resnet.avgpool
+
+        for layer1, layer2 in zip(list(model.encoder.children())[1:], list(model_resnet.children())[1:]):
+            layer1.load_state_dict(layer2.state_dict())
+        model.segmentation_head[2].activation = nn.Sigmoid()
+
+    model.to(device)
+    model = model.float()
+        
+    return model
+
+
+def train_model(train_set: Dataset, val_set: Dataset, arch: str, params: dict) -> nn.Module:
+    dataloaders = {
+        'train': create_dataloader(train_set, True),
+        'val': create_dataloader(val_set, False)
+    }
+    
+    model = create_model(arch, val_set[0][0].shape[0])
+    
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.BCELoss()
+    
+    metrics = {
+        'train': {'loss': [], 'iou': []},
+        'val': {'loss': [], 'iou': []}
+    }
+    
+    for phase in dataloaders:
+        for img, mask in dataloaders[phase]:
+            img = img.to(device)
+            mask = mask.to(device)
+            
+            out = model(img)
+            loss = criterion(out, mask)
+            iou = jaccard_index(out, mask)
+            
+            metrics[phase]['loss'].append(loss)
+            metrics[phase]['iou'].append(iou)
+            
+            if phase == 'train':
+                optimizer.zero_grad()
+                optimizer.step()
+                loss.backward()
+    
+    return model, metrics
+
+
+def run_inference(dataset: Dataset, model: nn.Module, save_dir: str):
+    dataloader = create_dataloader(dataset, is_train=False)
+    
+    for img, path in tqdm(dataloader):
+        img = img.to(device)
+        
+        out = model(img.unsqueeze()).squeeze().cpu().numpy()
+        np.save(save_dir, os.path.join(save_dir, os.path.basename(path)), out)
+        
+
+def load_model(model_path: str) -> nn.Module:
+    model = torch.load(model_path)
+    model.to(device)
+    
+    return model
